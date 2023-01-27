1a7213fb0d88dd2660059ae5213ead98b5ae2c9d
Dênis Augusto Luft <denisluft8@gmail.com>
2023-01-27 14:14:15 -0300

    Refeito JS, correção de estilo e remoção de direcionais.

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const slideRight = document.getElementById("slide-right");
-const slideLeft = document.getElementById(".slide-left");
 const mensal = document.getElementById("toggle-on");
 const anual = document.getElementById("toggle-off");
 let valorTotal = document.querySelector(".valor-total");
 let slideValue = document.querySelector(".slide__value");
 let slideInput = document.querySelector(".slide__input");
-let valorT = parseInt(slideInput.getAttribute("value"));
 let tipoPlano = false;
 
 function slider() {
-  valorT = parseInt(slideInput.getAttribute("value"));
-  console.log(valorT);
   slideInput.setAttribute("value", slideInput.value);
   slideValue.innerHTML = slideInput.getAttribute("value");
   valorTotal.innerHTML =
@@ -19,21 +14,16 @@ function slider() {
       : `R$${slideInput.value * 100},00`;
 }
 
-function addValue() {
-  const valueToAdd = parseInt(valorT) + 1;
-  valorT = valueToAdd;
-  slideInput.setAttribute("value", valueToAdd);
-  console.log(valueToAdd);
-  slideInput.setAttribute("value", valueToAdd);
-  slideValue.innerHTML = slideInput.getAttribute("value");
-  valorTotal.innerHTML =
-    tipoPlano == true
-      ? `R$${slideInput.value * 1200},00`
-      : `R$${slideInput.value * 100},00`;
+function planoMensal() {
+  tipoPlano = false;
+  valorTotal.innerHTML = `R$${slideInput.value * 100},00`;
+}
+function planoAnual() {
+  tipoPlano = true;
+  valorTotal.innerHTML = `R$${slideInput.value * 1200},00`;
 }
 
-anual.addEventListener("click", () => (tipoPlano = true));
-mensal.addEventListener("click", () => (tipoPlano = false));
+mensal.addEventListener("click", planoMensal);
+anual.addEventListener("click", planoAnual);
 
 slideInput.addEventListener("input", slider);
-slideRight.addEventListener("click", addValue);
