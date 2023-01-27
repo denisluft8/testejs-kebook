a87a71912bf87ffd83d814dec3937422e5a97adc
Dênis Augusto Luft <denisluft8@gmail.com>
2023-01-27 12:35:17 -0300

    alteração de javascript e css

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+const slideRight = document.getElementById("slide-right");
+const slideLeft = document.getElementById(".slide-left");
+const mensal = document.getElementById("toggle-on");
+const anual = document.getElementById("toggle-off");
+let valorTotal = document.querySelector(".valor-total");
 let slideValue = document.querySelector(".slide__value");
-const slideRight = document.querySelector(".slideRight");
-const slideLeft = document.querySelector(".slideLeft");
-let slideInput = (document.querySelector(".slide__input").oninput =
-  function () {
-    let value = ((this.value - this.min) / (this.max - this.min)) * 10;
-    this.style.background = `linear-gradient(to right, #ff18ca 0%, #f5a901 ${
-      value * 10
-    }%, #EEEEF0 ${value * 10}%,  #d4d4d9 100%)`;
-    slideValue.innerHTML = this.value;
-  });
+let slideInput = document.querySelector(".slide__input");
+let valorT = parseInt(slideInput.getAttribute("value"));
+let tipoPlano = false;
+
+function slider() {
+  valorT = parseInt(slideInput.getAttribute("value"));
+  console.log(valorT);
+  slideInput.setAttribute("value", slideInput.value);
+  slideValue.innerHTML = slideInput.getAttribute("value");
+  valorTotal.innerHTML =
+    tipoPlano == true
+      ? `R$${slideInput.value * 1200},00`
+      : `R$${slideInput.value * 100},00`;
+}
+
+function addValue() {
+  const valueToAdd = parseInt(valorT) + 1;
+  valorT = valueToAdd;
+  slideInput.setAttribute("value", valueToAdd);
+  console.log(valueToAdd);
+  slideInput.setAttribute("value", valueToAdd);
+  slideValue.innerHTML = slideInput.getAttribute("value");
+  valorTotal.innerHTML =
+    tipoPlano == true
+      ? `R$${slideInput.value * 1200},00`
+      : `R$${slideInput.value * 100},00`;
+}
+
+anual.addEventListener("click", () => (tipoPlano = true));
+mensal.addEventListener("click", () => (tipoPlano = false));
+
+slideInput.addEventListener("input", slider);
+slideRight.addEventListener("click", addValue);
